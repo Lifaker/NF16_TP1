a8b36ee982b701b5b01c7220d10e9ccd70478054
Lifaker <enchanter111@gmail.com>
2017-03-15 14:23:24 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Ex1.h"
+#include "Ex2.h"
+#include "Ex3.h"
+#include "Ex4.h"
+#include "Ex5.h"
+
+int main()
+{
+/* Exercice 1 */
+    //somme();
+    //inverser();
+    //pourcentage();
+    //temperature();
+    //conversion();
+    //pair();
+    //ascii();
+
+/* Exercice 2 */
+    //test_ascii();
+    //saisie_tab();
+    //table_multiple();
+    //nb_lettre();
+
+/* Exercice 3 */
+    //init_table5();
+    //double_table();
+    //multip_matrice();
+
+/* Exercice 4 */
+    /*float a,b;
+    printf("Entrez 2 nombres : ");
+    scanf("%f %f", &a, &b);
+    permut(&a,&b);
+    printf("a = %f  b = %f ", a, b);
+    return 0;*/
+    //pointeurs();
+    //lecture();
+
+/* Exercice 5 */
+    //sous_chaine();
+    majuscule();
+}
